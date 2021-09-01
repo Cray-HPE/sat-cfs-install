1e5dcd0c9113662330ea4087b0cf8f0efe2673e3
Pat Tovo <patricia.tovo@hpe.com>
2021-09-01 11:08:42 -0500

    CRAYSAT-1104: Update cf-gitea-import to address OpenSSL CVE (2021_3711)

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -12,10 +12,4 @@ sed -i s/@config_image_name@/${config_image_name}/g kubernetes/sat-cfs-install/v
 # Set the product name
 sed -i s/@product_name@/sat/g kubernetes/sat-cfs-install/values.yaml
 
-# Set the cf-gitea-import image version, which is the base image for the sat-cfs-install Docker image.
-wget https://arti.dev.cray.com/artifactory/csm-misc-stable-local/manifest/cf-gitea-import-manifest.txt
-cf_gitea_import_image_tag=$(grep cf-gitea-import cf-gitea-import-manifest.txt | sed s/.*://g | tr -d '[:space:]')
-sed -i s/@cf_gitea_import_image_tag@/${cf_gitea_import_image_tag}/g Dockerfile
-rm cf-gitea-import-manifest.txt
-
 cat kubernetes/sat-cfs-install/values.yaml
