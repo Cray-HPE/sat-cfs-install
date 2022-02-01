41f2927377d66875e83c1863a8fffb1f1051c67f
Ryan Haasken <haasken@cray.com>
2022-01-31 18:09:38 -0600

    CRAYSAT-1177: Enable GPG checking RPMs on release branch
    Enable GPG checking on RPMs on release branch builds. Only enable GPG
    checking on RPMs, not on the repository metadata, which is not signed.
    If this is not a release branch build, the RPMs will not be signed, so
    disable GPG checks entirely on non-release-branch builds.
    
    The `runBuildPrep.sh` script is used to control whether GPG checking is
    enabled or disabled on the repos by substituting in the appropriate
    values in the Ansible role's `defaults/main.yml`.
    
    Test Description:
    Ran runBuildPrep.sh locally and copied resulting Ansible role to a
    system that has a master-branch build with unsigned RPMs (hela),
    uninstalled the cray-sat-podman RPM on a master node and
    updated the SAT layer of ncn-personalization to point at a branch with
    the new content. Ensured that the cray-sat-podman RPM was installed.
    
    Did the same but built locally on a branch that starts with "release/"
    and tested on a system installed with a SAT 2.2 release branch build of
    SAT (baldar). Confirmed that the RPM was installed, and GPG checking on
    the packages was enabled on the repository.

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # Build preparation for sat-cfs-install.
 #
@@ -7,9 +7,17 @@
 # Set the docker image name for the config image
 config_image_name=${IMAGE_NAME}
 echo "config_image_name=${config_image_name}"
-sed -i s/@config_image_name@/${config_image_name}/g kubernetes/sat-cfs-install/values.yaml
+sed -i "s/@config_image_name@/${config_image_name}/g" kubernetes/sat-cfs-install/values.yaml
 
 # Set the product name
 sed -i s/@product_name@/sat/g kubernetes/sat-cfs-install/values.yaml
 
 cat kubernetes/sat-cfs-install/values.yaml
+
+# Only enable GPG checking on release branch; RPMs only get signed on release branches
+disable_gpg_check=yes
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$branch_name" == release/* ]]; then
+    disable_gpg_check=no
+fi
+sed -i "s/@disable_gpg_check@/${disable_gpg_check}/" ansible/roles/sat-ncn/defaults/main.yml
